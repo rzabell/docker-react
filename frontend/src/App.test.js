33197140de65d0b6bc21850a15e341dbc06c602f
rzabell <rzabell315@gmail.com>
2020-08-28 13:33:33 -0400

    initial commit

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,13 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-it('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-it('renders learn react link', () => {
+test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
